c46a9f2c268a848023d65aea117db689284ed75b
Krittika123 <kaal.kk5@gmail.com>
2022-04-11 16:41:54 +0100

    my first commit krizband

---
+++ src/Home.jsx
@@ -0,0 +1,356 @@
+import React from "react";
+function Home(){
+    return(
+        <>
+        <div className="container-fluid" style={{backgroundImage:"url('https://technext.github.io/mirko/assets/images/home.jpg')",height:"900px",width:"100%",backgroundSize:"cover"}}>
+        <nav className="navbar navbar-expand-md">
+      <img height="100" width="150" src="https://i.pinimg.com/originals/0c/e4/8c/0ce48c67b1907210551c22fc56514034.png" className="navbar-brand"/>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data target="#Krittika">
+        <span className="navbar-toggler-icon"></span> 
+        </button>
+         
+        <div className="collapse navbar-collapse justify-content-end" id="Krittika">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Service</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Plans</a>
+            </li> 
+            <li className="nav-item"> 
+              <a className="nav-link text-white" href="#">Drop</a>
+            </li> 
+            <li className="nav-item"> 
+              <a className="nav-link text-white" href="#">Contact</a>
+            </li>
+            
+          </ul>
+        </div>
+  </nav> 
+  <br/><br/><br/><br/><br/><br/>
+  <br/>
+  <div className="container text-white">
+      <h1 className="display-3"><b> Best <span style={{color:"lightgreen"}}>Broadband</span><br/>
+      Internet Plans for you</b>
+      </h1>
+      <p>Mind-blowing speeds of up to 900 Mb/s with no need landline! You can download and 
+      stream HD and 4K videos and box sets faster than ever before.</p>
+      <p>Get Unlimited Broadband and Top Speeds from only £17.95 Per Month</p>
+      <p>Find Your Perfect Package And Save Today!</p>
+      <br/><br/>
+      <button type="button" className="btn btn-outline-success btn-lg">View Plans</button>
+    </div>
+  </div>
+
+  <br /><br />
+
+    <div class="card-group text-white">
+
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+              <p>Download 1 GBPS</p>
+              <p>Wherever you are, we've got a broadband package for you. It's broadband you can rely on. <br />
+              Get guaranteed wi-fi in every room or £100 back with Complete Wi-Fi   
+              </p>
+            </div>
+        </div>
+
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+              <p>99% Internet Uptime</p>
+              <p>Krizband gives you mind-blowing speeds of up to 900 Mb/s with no need for a landline. <br />
+              You can download and stream HD and 4K videos and box sets faster.
+              </p>
+            </div>
+        </div>
+
+        <div class="card bg-dark">
+            <div class="card-body text-center">
+              <p>24/7 Support </p>
+              <p>With average speeds of 108Mbps, discover our super-fast fibre broadband offer for £25/m. <br />
+              Shop truly unlimited, reliable broadband with Krizband.
+              </p>
+            </div>
+        </div>
+
+    </div>
+<br /><br /><br />
+    
+    <div class="card-group">
+    
+      <div class="card bg-dark text-white">
+        <div class="card-body">
+          <h4>ABOUT US</h4>
+          <h1><b>We are top internet <br /> service company</b></h1>
+          <p>Broadband Spring Fibre Special From Only £25/m For 18 months With £19.95 Upfront Fee. Superfast Speeds, Amazing Value & 
+          Service From UKs Top Performing Major Broadband Provider. Switching Is Easy. Perfect For Streaming. Broadband Superfast.
+          Join A Network That Cares, Excellent Rated 365 Days UK Support With Our Superfast Fibre.</p>
+          <button type="button" className="btn btn-outline-success btn-lg">Learn More</button>
+        </div>
+      </div>
+          
+        <div class="card bg-dark text-white">
+          <div class="card-body">
+            <img className="rounded-circle" src="https://technext.github.io/mirko/assets/images/about.jpg" alt="" />
+          </div>
+        </div>
+      
+        </div>
+
+        <br /><br /><br /> 
+      <div className="container-fluid">
+        <div class="card-deck">
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+              <h2>HOME BROADBAND</h2>
+              <p>BT offers home broadband, phone and TV packages in the UK, along with a mobile service. Its fibre optic services reach speeds up to 900Mbps. </p>
+              </div>
+            </div>
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+              <h2>HOME WIFI</h2>
+              <p>Stream, Game & More With Our Super Reliable Broadband With Average Speeds Of 35Mb/s. Using Krizband - Pause, Record, Rewind Or Restart Live TV. All In One Place, Easy. Install With Confidence. Record And Store More. Watch TV Around Your Home.</p>
+              </div>
+            </div>
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+                <h2>HOME LANDLINE</h2>
+                <p>Our Best Deals on Home Phone; Landline. Cheap Home Phone Offers - the Cheapest Calls; Landline Discounts</p>
+              </div>
+            </div>
+
+        </div>
+        </div>
+
+    <br /><br />  
+          <div class="card-deck">
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+              <h2>MOBILE CONNECTION</h2>
+              <p>Mobile broadband is the ability to connect your laptop or tablet to the web by using 3G, 4G or 5G phone networks.</p>
+              </div>
+            </div>
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+              <h2>SECURITY</h2>
+              <p>With broadband security software you can protect your computer when browsing the web or downloading</p>
+              </div>
+            </div>
+
+            <div class="card bg-dark text-white">
+              <div class="card-body" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+                <h2>TV SETUP BOX</h2>
+                <p>200+ live channels including Freeview, 50+ in HD! 40,000 hours of on-demand content. Choice of add-ons (BT Sport, Rakuten TV, BritBox, hayu, and more</p>
+              </div>
+            </div>
+        </div>
+
+
+
+      <br /><br /><br />
+        <div className="text-center container-fluid" style={{fontSize:"23px"}}>
+            <h4>OUR PLANS</h4>
+            <h1><b>Explore unlimited possibilities</b></h1>
+            <p>Best offers and packages here!</p>
+            
+            
+            <div class="card-deck">
+
+              <div className="card">
+                <div class="card-body text-center bg-dark" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+                  <h2>Basic Bundle</h2>
+                  <ul>
+                    <li>Easy Installment</li>
+                    <li>WiFi Hub</li>
+                    <li>Unlimited usage</li>
+                    <li>Superfast avg speeds (100 Mbps)</li> 
+                  </ul>
+                  <h3>£24/month</h3>
+                  <button type="button" className="btn btn-outline-success btn-lg">View Plans</button>
+                </div>
+              </div>
+              
+              <div className="card">
+                <div class="card-body text-center bg-dark"  style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+                  <h2>Business Bundle</h2>
+                  <ul>
+                    <li>12 months free Premium antivirus</li>
+                    <li>4G Broadband Back-up</li>
+                    <li>Discount for Krizband mobile customers</li>
+                    <li>Security Cameras</li>
+                  </ul>
+                  <h3>£99/month</h3>
+                  <button type="button" className="btn btn-outline-success btn-lg">View Plans</button>
+                </div>
+              </div>
+
+              <div className="card">
+                <div class="card-body text-center bg-dark" style={{borderStyle:"solid",borderWidth:"2px",borderColor:"white"}}>
+                  <h2>Premium Bundle</h2>
+                  <ul>
+                    <li>Plus with Alexa Built-in</li>
+                    <li>Anytime landline and mobile calls</li>
+                    <li>Super WiFi & Guarantee</li>
+                    <li>Faster Fibre Promise</li>
+                  </ul>
+                  <h3>£199/month</h3>
+                  <button type="button" className="btn btn-outline-success btn-lg">View Plans</button>
+                </div>
+              </div>
+      
+        </div>
+</div>
+
+<br/><br /><br />
+
+            <div className="container-fluid" style={{backgroundColor:"black",color:"white", fontSize:"25px"}}>
+            <div class="card-group">
+
+              <div class="card">
+                <div class="card-body text-center bg-dark">
+                  <img src="https://technext.github.io/mirko/assets/images/work.jpg" alt="" height="450" width="700"/>
+                </div>
+              </div>
+
+              <div class="card">
+                <div class="card-body bg-dark text-white">
+                  <p>OUR WORK</p>
+                  <h2>Explore unlimited possibilities</h2>
+                  <ul>
+                    <li>1258</li>PROJECTS COMPLETED
+                    <li>150</li>AWARDS
+                    <li>1255</li>CUSTOMER ACTIVE
+                    <li>1157</li>REVIEWS
+                  </ul>
+                </div>
+              </div>
+
+            </div>
+            </div>
+
+            <br /><br /><br />
+
+            <div class="container">
+              <p>TESTIMONIALS</p>
+              <h1>What our clients say</h1>
+
+              <div class="card-deck">
+
+                <div class="card bg-dark" style={{borderStyle:"solid",borderWidth:"5px",borderColor:"green"}}>
+                  <div class="card-body">
+                    <h4>Love Krizband. Very fast and speedy wifi for my work. Securit cameras work great.</h4>
+                    <img className="rounded-circle " height="80" width="80" src="https://technext.github.io/mirko/assets/images/testimonial-2.jpg" alt="" />
+                  </div>
+                </div>
+
+                <div class="card bg-dark" style={{borderStyle:"solid",borderWidth:"5px",borderColor:"green"}}>
+                  <div class="card-body">
+                    <h4>Very reliable. Price stays the same all the time. You should go for this.</h4>
+                    <img className="rounded-circle" height="80" width="80" src="https://technext.github.io/mirko/assets/images/testimonial-3.jpg" alt="" />
+                  </div>
+                </div>
+
+                <div class="card bg-dark" style={{borderStyle:"solid",borderWidth:"5px",borderColor:"green"}}>
+                  <div class="card-body">
+                    <h4>My kids love to play an are very happy because it loads very quickly.</h4>
+                    <img className="rounded-circle" height="80" width="80" src="https://technext.github.io/mirko/assets/images/testimonial-1.jpg" alt="" />
+                  </div>
+                </div>
+    
+              </div>
+            </div>
+
+            <br /><br />
+            
+            <div className="container">
+
+              <div class="card-deck">
+
+                <div class="card bg-dark">
+                  <div class="card-body text-center">
+                  
+                  <p>CONTACT</p>
+                  <h1>Send Your Query</h1>
+                  <h5>If you have a problem fill out this form and send us an email.</h5>
+
+                  <form> 
+                    <input type="text" class="form-control" placeholder="Enter email"/>
+                    <br />
+                    <input type="number" class="form-control" placeholder="Enter Phone Number"/>
+                    <br />
+                    <input type="text" class="form-control" placeholder="Enter Email"/>
+                    <br />
+                    <textarea className="form-control" rows="5" placeholder="Message" />
+                    <br />
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form> 
+                  
+                </div>
+                
+                </div>
+
+              <div class="card bg-dark">
+                <div class="card-body text-center">
+                  <img height="450" width="500" src="https://technext.github.io/mirko/assets/images/contact.jpg" alt="" />
+                </div>
+              </div>
+
+              </div>
+
+              <br /><br />
+
+            <div className="container-fluid">  
+
+              <div class="card-deck">
+
+                <div class="card bg-dark">
+                  <div class="card-body text-center">
+                    <p><b>ADDRESS</b></p>
+                    <p>Queen's road 23</p>
+                  </div>
+                </div>
+
+                <div class="card bg-dark">
+                  <div class="card-body text-center">
+                    <p><b>CALL FOR QUERY</b></p>
+                    <p>(800) 265 216 2020</p>
+                  </div>
+                </div>
+
+                <div class="card bg-dark">
+                  <div class="card-body text-center">
+                    <p><b>SEND US A MESSAGE</b></p>
+                    <p>infodemofile@example.com</p>
+                  </div>
+                </div>
+
+                <div class="card bg-dark">
+                  <div class="card-body text-center">
+                    <p><b>OPENING HOURS</b></p>
+                    <p>09:00 AM - 18:00 PM</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <br /><br />
+
+
+              </div>
+        </>
+    );
+
+}
+
+export default Home;
